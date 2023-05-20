c881dfd19f518ac2c336717ecc5584d8c60cce7e
NurNajwa-1509 <97671680+NurNajwa-1509@users.noreply.github.com>
2023-05-20 16:19:23 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var bgimg,bg;
+var carrotseedimg,potatoseedimg
+
+function preload(){
+bgimg=loadImage("assets/splash.gif")
+
+}
+
+
+function setup(){
+createCanvas(windowWidth,windowHeight)
+
+}
+
+function draw(){
+background(bgimg)
+}
